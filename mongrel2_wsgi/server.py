7df800fdc00b4219f0f79ca514acc3dac3bdd00c
Timothy Fitz <TimothyFitz@gmail.com>
2010-09-05 19:32:18 -0700

    Rename environ -> env.

--- mongrel2_wsgi/server.py
+++ mongrel2_wsgi/server.py
@@ -49,44 +49,44 @@ def wsgi_server(application, conn):
             if DEBUG: print "DISCONNECT"
             continue #effectively ignore the disconnect from the client
         
-        # Set a couple of environment attributes a.k.a. header attributes 
+        # Set a couple of envment attributes a.k.a. header attributes 
         # that are a must according to PEP 333
         req.headers = dict((key.encode('ascii'), value.encode('ascii')) for (key,value) in req.headers.items())
-        environ = {}
-        environ['SERVER_PROTOCOL'] = 'HTTP/1.1' # SimpleHandler expects a server_protocol, lets assume it is HTTP 1.1
-        environ['REQUEST_METHOD'] = req.headers['METHOD']
+        env = {}
+        env['SERVER_PROTOCOL'] = 'HTTP/1.1' # SimpleHandler expects a server_protocol, lets assume it is HTTP 1.1
+        env['REQUEST_METHOD'] = req.headers['METHOD']
         if ':' in req.headers['Host']:
-            environ['SERVER_NAME'] = req.headers['Host'].split(':')[0]
-            environ['SERVER_PORT'] = req.headers['Host'].split(':')[1]
+            env['SERVER_NAME'] = req.headers['Host'].split(':')[0]
+            env['SERVER_PORT'] = req.headers['Host'].split(':')[1]
         else:
-            environ['SERVER_NAME'] = req.headers['Host']
-            environ['SERVER_PORT'] = ''
-        environ['SCRIPT_NAME'] = '' # empty for now
+            env['SERVER_NAME'] = req.headers['Host']
+            env['SERVER_PORT'] = ''
+        env['SCRIPT_NAME'] = '' # empty for now
 		# 26 aug 2010: Apparently Mongrel2 has started (around 1.0beta1) to quote urls and
 		# apparently Django isn't expecting an already quoted string. So, I just
 		# unquote the path_info here again so Django doesn't throw a "page not found" on 
 		# urls with spaces and other characters in it.
-        environ['PATH_INFO'] = urllib.unquote(req.headers['PATH'])
+        env['PATH_INFO'] = urllib.unquote(req.headers['PATH'])
         if '?' in req.headers['URI']:
-            environ['QUERY_STRING'] = req.headers['URI'].split('?')[1]
+            env['QUERY_STRING'] = req.headers['URI'].split('?')[1]
         else:
-            environ['QUERY_STRING'] = ''
+            env['QUERY_STRING'] = ''
         if req.headers.has_key('Content-Length'):
-            environ['CONTENT_LENGTH'] = req.headers['Content-Length'] # necessary for POST to work with Django
-        environ['wsgi.input'] = req.body
+            env['CONTENT_LENGTH'] = req.headers['Content-Length'] # necessary for POST to work with Django
+        env['wsgi.input'] = req.body
         
         for k,v in req.headers.items():
             k=k.replace('-','_').upper(); v=v.strip()
-            if k in environ:
+            if k in env:
                 continue                    # skip content length, type,etc.
             http_k = 'HTTP_'+k
-            if http_k in environ:
-                environ[http_k] += ','+v     # comma-separate multiple headers
+            if http_k in env:
+                env[http_k] += ','+v     # comma-separate multiple headers
             else:
-                environ[http_k] = v
+                env[http_k] = v
         
         
-        if DEBUG: print "ENVIRON: %r\n" % environ
+        if DEBUG: print "ENVIRON: %r\n" % env
         
         # SimpleHandler needs file-like stream objects for
         # requests, errors and reponses
@@ -95,8 +95,32 @@ def wsgi_server(application, conn):
         respIO = StringIO.StringIO()
         
         # execute the application
-        simple_handler = SimpleHandler(reqIO, respIO, errIO, environ, multithread = False, multiprocess = False)
+        simple_handler = SimpleHandler(reqIO, respIO, errIO, env, multithread = False, multiprocess = False)
         simple_handler.run(application)
+        
+        class Response(object):
+            def start_response(status, headers, exc_info=None):
+                if exc_info is not None:
+                    raise exc_info[0], exc_info[1], exc_info[2]
+                captured[:] = [status, headers, exc_info]
+                return output.append
+                
+        
+        captured = []
+        output = []
+
+        app_iter = application(self.env, start_response)
+        if output or not captured:
+            try:
+                output.extend(app_iter)
+            finally:
+                if hasattr(app_iter, 'close'):
+                    app_iter.close()
+            app_iter = output
+        if catch_exc_info:
+            return (captured[0], captured[1], app_iter, captured[2])
+        else:
+            return (captured[0], captured[1], app_iter)
         
         # Get the response and filter out the response (=data) itself,
         # the response headers, 
